fc826b13e6511d4bcf9578e4636f38ea366b14d7
brent <jinhoshin319@gmail.com>
2020-07-20 23:01:12 +0900

    changes in id

--- app/src/main/java/com/example/pushnotificaiton/MainActivity.kt
+++ app/src/main/java/com/example/pushnotificaiton/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.pushnotificaiton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pushnotificaiton.utils.push.PushNotification
+import com.example.pushnotificaiton.utils.push.NotificationBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,44 +16,60 @@ class MainActivity : AppCompatActivity() {
         //show notification
 
         btn_display_basic_notification.setOnClickListener {
-            PushNotification.from(this)
+            NotificationBuilder.from(this)
                 .createNotificationBuilder("HelloWorld")
                 .setTitle(R.string.app_name)
                 .setContent(R.string.app_name)
                 .setSmallIcon(R.drawable.ic_baseline_add_circle_24)
-                .createChannel()
-                .notifyUser()
+                .createChannel("name", "description", NotificationBuilder.IMPORTANCE.DEFAULT, NotificationBuilder.IMPORTANCE.DEFAULT)
+                .notifyUser(1)
         }
 
         btn_display_bigPicture_notification.setOnClickListener {
-            PushNotification.from(this)
+            NotificationBuilder.from(this)
                 .createNotificationBuilder("HelloWorld")
                 .setTitle(R.string.app_name)
                 .setExpandable(true, R.drawable.ic_launcher_playstore)
                 .setLargeIcon(R.drawable.ic_launcher_playstore)
                 .setSmallIcon(R.drawable.ic_baseline_add_circle_24)
-                .createChannel("name", "description", PushNotification.IMPORTANCE.DEFAULT, PushNotification.IMPORTANCE.DEFAULT)
-                .notifyUser()
+                .createChannel("name", "description", NotificationBuilder.IMPORTANCE.DEFAULT, NotificationBuilder.IMPORTANCE.DEFAULT)
+                .notifyUser(2)
         }
 
         btn_display_notification_with_action.setOnClickListener {
-            PushNotification.from(this)
+            NotificationBuilder.from(this)
                 .createNotificationBuilder("HelloWorld")
                 .setTitle(R.string.app_name)
                 .setContent(R.string.common_random)
                 .setSmallIcon(R.drawable.ic_baseline_add_circle_24)
-                .createChannel()
+                .createChannel("name", "description", NotificationBuilder.IMPORTANCE.DEFAULT, NotificationBuilder.IMPORTANCE.DEFAULT)
                 .setActionButton(getString(R.string.app_name))
-                .notifyUser()
+                .notifyUser(3)
+        }
+
+        btn_display_notification_with_large_block_of_text.setOnClickListener {
+            NotificationBuilder.from(this)
+                .createNotificationBuilder("HelloWorld")
+                .setTitle(R.string.app_name)
+                .setContent(R.string.common_random)
+                .setSmallIcon(R.drawable.ic_baseline_add_circle_24)
+                .createChannel("name", "description", NotificationBuilder.IMPORTANCE.DEFAULT, NotificationBuilder.IMPORTANCE.DEFAULT)
+                .setBlockOfText(getString(R.string.common_random))
+                .setActionButton(getString(R.string.app_name))
+                .notifyUser(4)
         }
 
         btn_display_notification_with_picture_and_action.setOnClickListener {
-            PushNotification.from(this)
-                .createNotificationBuilder("HelloWorld")
+            NotificationBuilder.from(this)
+                .createNotificationBuilder("Hello World")
                 .setTitle(R.string.app_name)
+                .setContext("this is how it's done")
                 .setSmallIcon(R.drawable.ic_baseline_add_circle_24)
-                .createChannel()
-                .notifyUser()
+                .setLargeIcon(R.drawable.ic_launcher_playstore)
+                .setExpandable(true, R.drawable.ic_launcher_playstore)
+                .setActionButton(getString(R.string.app_name))
+                .createChannel("name", "description", NotificationBuilder.IMPORTANCE.DEFAULT, NotificationBuilder.IMPORTANCE.DEFAULT)
+                .notifyUser(5)
         }
     }
 }
